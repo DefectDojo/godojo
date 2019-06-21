b8e37827b41bca24331deebb754deb4d896fffe7
Matt Tesauro <mtesauro@gmail.com>
2019-06-20 22:38:46 -0500

    POC'ed env variables overriding config values, updated config file

--- config/installConfig.go
+++ config/installConfig.go
@@ -1,90 +1,95 @@
-package main
+package config
 
-type dojoConfig struct {
-	install  installConfig
-	settings settingsConfig
+type DojoConfig struct {
+	Install  InstallConfig
+	Settings SettingsConfig
 }
 
-type installConfig struct {
+type InstallConfig struct {
 	// Installer settings
-	ddDojoVer     string // Holds the version of Dojo to check out from the repo
-	ddPrompt      bool
-	ddInstallType string
-	ddDBType      string
+	Version     string // Holds the version of Dojo to check out from the repo
+	Prompt      bool
+	InstallType string
+	DB          DBTarget
 }
 
-type settingsConfig struct {
+type DBTarget struct {
+	Engine string
+	Local  bool
+}
+
+type SettingsConfig struct {
 	// Configs for settings.py
-	ddDebug                         bool   // Run Dojo in debug mode, default false
-	ddLoginRedirectURL              string // Where to redirect to after login, default "/"
-	ddDjangoAdminEnabled            bool   // Is Django's admin enabled, default false
-	ddSessionCookieHttponly         bool   // Add httponly cookie option to session, default true
-	ddCsrfCookieHttponly            bool   // Add httponly cookie option to csrf, default true
-	ddSecureSslRedirect             bool   // Redirect to https, default false
-	ddSecureHstsInclueSubdomains    bool   // Add HSTS header to include subdomains
-	ddSecureHstsSeconds             int    // Number of seconds for HSTS, default 31536000 aka 1 year
-	ddCsrfCookieSecure              bool   // CSRF cookie has secure flag, default false
-	ddSecureBrowserXSSFilter        bool   // TODO, default false
-	ddTimeZone                      string // Time zone for Defect Dojo, default UTC
-	ddLang                          string // Default language for Dojo, default en-us
-	ddWkhtmltopdf                   string // path to wkhtmltopdf binary, default /usr/local/bin/wkhtmltopdf
-	ddTeamName                      string // Name of the team in dojo, default "Security Team"
-	ddAdmins                        string // admin email addresses, default "DefectDojo:dojo@localhost,Admin:admin@localhost"
-	ddPortScanContactEmail          string // email address for port scans, default "email@localhost"
-	ddPortScanResultEmailFrom       string // email address port scan results are sent from, default "email@localhost"
-	ddPortScanExternalUnitEmailList string // TODO, default "email@localhost"
-	ddPortScanSourceIP              string // What souce IP to use for port scanning, default "127.0.0.1"
-	ddWhitenoise                    bool   // Should Whitenoise be used, default false
-	ddTrackMigrations               bool   // TODO, default false
-	ddSecureProxySslHeader          bool   // TODO, default false
-	ddTestRunner                    string // TODO, default "django.test.runner.DiscoverRunner"
-	ddURLPrefix                     string // Prefix to the Dojo URL, default ""
-	ddRoot                          string // TODO, default "dojo"
-	ddLanguageCode                  string // TODO, default "en-us"
-	ddSiteID                        int    // TODO, default 1
-	ddUseI18N                       bool   // TODO, default true
-	ddUseL10n                       bool   // TODO, default true
-	ddUseTz                         bool   // TODO, default true
-	ddMediaURL                      string // URL prefix for Dojo media files, default "/media/"
-	ddMediaRoot                     string // File path to the directory to store media files, default ddRoot + "media"
-	ddStaticURL                     string // URL prefix for Dojo static files, default "/static/"
-	ddStaticRoot                    string // File path to the directory to store static files, default ddRoot + "static"
-	ddCeleryBrokerURL               string // TODO, default ""
-	ddCeleryBrokerScheme            string // TODO, default "sqla+sqlite"
-	ddCeleryBrokerUser              string // TODO, default ""
-	ddCeleryBrokerPassword          string // TODO, default ""
-	ddCeleryBrokerHost              string // TODO, default ""
-	ddCeleryBrokerPort              int    // TODO, default -1
-	ddCeleryBrokerPath              string // TODO, default "/dojo.celerydb.sqlite"
-	ddCeleryTaskIgnoreResult        bool   // TODO, default true
-	ddCeleryResultBackend           bool   // TODO, default "django-db"
-	ddCeleryResultExpires           int    // TODO, default 86400 aka 24 hours
-	ddCeleryBeatScheduleFilename    string // TODO, default ddRoot + "dojo.celery.beat.db"
-	ddCeleryTaskSerializer          string // TODO, default "pickle"
-	ddForceLowercaseTags            bool   // Force all tags to be lowercase, default true
-	ddMaxTagLength                  int    // Max length for a tag in Dojo, default 25
-	ddDatabaseEngine                string // Database for Dojo to use, default "django.db.backends.mysql"
-	ddDatabaseHost                  string // Database host name, default "mysql"
-	ddDatabaseName                  string // Database name for Dojo to use, default "defectdojo"
-	ddTestDatabaseName              string // Database name used for testing, default "test_defectdojo"
-	ddDatabasePassword              string // Dojo database user password, default "defectdojo"
-	ddDatabasePort                  int    // Port that database is listening on, default 3306
-	ddDatabaseUser                  string // Database user for the Dojo database, default "defectdojo"
-	ddSecretKey                     string // TODO, default "."
-	ddCredentialAES256Key           string // TODO, default "."
-	ddDataUploadMaxMemorySize       int    // Max post size, default 8388608 aka 8 MB
-	ddSocialAuthGoogleOAuth2Key     string // TODO, default ""
-	ddSocialAuthGoogleOAuth2Secret  string // TODO, default ""
-	ddSocialAuthOktaOAuth2Key       string // TODO, default ""
-	ddSocialAuthOktaOAuthSecret     string // TODO, default ""
-	ddSocialAuthOktaOAuthAPIURL     string // TODO, default "https://{your-org-url}/oauth2/default"
+	Debug                         bool   // Run Dojo in debug mode, default false
+	LoginRedirectURL              string // Where to redirect to after login, default "/"
+	DjangoAdminEnabled            bool   // Is Django's admin enabled, default false
+	SessionCookieHttponly         bool   // Add httponly cookie option to session, default true
+	CsrfCookieHttponly            bool   // Add httponly cookie option to csrf, default true
+	SecureSslRedirect             bool   // Redirect to https, default false
+	SecureHstsInclueSubdomains    bool   // Add HSTS header to include subdomains
+	SecureHstsSeconds             int    // Number of seconds for HSTS, default 31536000 aka 1 year
+	CsrfCookieSecure              bool   // CSRF cookie has secure flag, default false
+	SecureBrowserXSSFilter        bool   // TODO, default false
+	TimeZone                      string // Time zone for Defect Dojo, default UTC
+	Lang                          string // Default language for Dojo, default en-us
+	Wkhtmltopdf                   string // path to wkhtmltopdf binary, default /usr/local/bin/wkhtmltopdf
+	TeamName                      string // Name of the team in dojo, default "Security Team"
+	Admins                        string // admin email addresses, default "DefectDojo:dojo@localhost,Admin:admin@localhost"
+	PortScanContactEmail          string // email address for port scans, default "email@localhost"
+	PortScanResultEmailFrom       string // email address port scan results are sent from, default "email@localhost"
+	PortScanExternalUnitEmailList string // TODO, default "email@localhost"
+	PortScanSourceIP              string // What souce IP to use for port scanning, default "127.0.0.1"
+	Whitenoise                    bool   // Should Whitenoise be used, default false
+	TrackMigrations               bool   // TODO, default false
+	SecureProxySslHeader          bool   // TODO, default false
+	TestRunner                    string // TODO, default "django.test.runner.DiscoverRunner"
+	URLPrefix                     string // Prefix to the Dojo URL, default ""
+	Root                          string // TODO, default "dojo"
+	LanguageCode                  string // TODO, default "en-us"
+	SiteID                        int    // TODO, default 1
+	UseI18N                       bool   // TODO, default true
+	UseL10n                       bool   // TODO, default true
+	UseTz                         bool   // TODO, default true
+	MediaURL                      string // URL prefix for Dojo media files, default "/media/"
+	MediaRoot                     string // File path to the directory to store media files, default ddRoot + "media"
+	StaticURL                     string // URL prefix for Dojo static files, default "/static/"
+	StaticRoot                    string // File path to the directory to store static files, default ddRoot + "static"
+	CeleryBrokerURL               string // TODO, default ""
+	CeleryBrokerScheme            string // TODO, default "sqla+sqlite"
+	CeleryBrokerUser              string // TODO, default ""
+	CeleryBrokerPassword          string // TODO, default ""
+	CeleryBrokerHost              string // TODO, default ""
+	CeleryBrokerPort              int    // TODO, default -1
+	CeleryBrokerPath              string // TODO, default "/dojo.celerydb.sqlite"
+	CeleryTaskIgnoreResult        bool   // TODO, default true
+	CeleryResultBackend           bool   // TODO, default "django-db"
+	CeleryResultExpires           int    // TODO, default 86400 aka 24 hours
+	CeleryBeatScheduleFilename    string // TODO, default ddRoot + "dojo.celery.beat.db"
+	CeleryTaskSerializer          string // TODO, default "pickle"
+	ForceLowercaseTags            bool   // Force all tags to be lowercase, default true
+	MaxTagLength                  int    // Max length for a tag in Dojo, default 25
+	DatabaseEngine                string // Database for Dojo to use, default "django.db.backends.mysql"
+	DatabaseHost                  string // Database host name, default "mysql"
+	DatabaseName                  string // Database name for Dojo to use, default "defectdojo"
+	TestDatabaseName              string // Database name used for testing, default "test_defectdojo"
+	DatabasePassword              string // Dojo database user password, default "defectdojo"
+	DatabasePort                  int    // Port that database is listening on, default 3306
+	DatabaseUser                  string // Database user for the Dojo database, default "defectdojo"
+	SecretKey                     string // TODO, default "."
+	CredentialAES256Key           string // TODO, default "."
+	DataUploadMaxMemorySize       int    // Max post size, default 8388608 aka 8 MB
+	SocialAuthGoogleOAuth2Key     string // TODO, default ""
+	SocialAuthGoogleOAuth2Secret  string // TODO, default ""
+	SocialAuthOktaOAuth2Key       string // TODO, default ""
+	SocialAuthOktaOAuthSecret     string // TODO, default ""
+	SocialAuthOktaOAuthAPIURL     string // TODO, default "https://{your-org-url}/oauth2/default"
 }
 
-func testConfig() string {
+func TestConfig() string {
 	return "Worked"
 }
 
-func readInstallConfig() error {
+func ReadInstallConfig() error {
 	// Read the config
 
 	// Set the default values
--- godojo.go
+++ godojo.go
@@ -5,14 +5,13 @@ package main
 // Add go-git to pull Dojo source - https://github.com/src-d/go-git
 import (
 	"fmt"
+	"strings"
 
+	"github.com/mtesauro/godojo/config"
 	"github.com/spf13/viper"
 )
 
 // TODO: Consider moving install-config.go into a subfolder and import it abovj
-//type installConfig struct {
-//	dojoVer string
-//}
 
 // getDojo retrives the supplied version of DefectDojo from the Git repo
 // and places it in the specified dojoSource directory (default is /opt/dojo)
@@ -27,7 +26,7 @@ func main() {
 	fmt.Printf("ver = %+v\n", ver)
 
 	// Read the config and set default values
-	err := readInstallConfig()
+	err := config.ReadInstallConfig()
 	if err != nil {
 		panic("Can't read config nor setup default values")
 	}
@@ -35,7 +34,16 @@ func main() {
 	// Setup viper config
 	viper.AddConfigPath(".")
 	viper.SetConfigName("dojoConfig")
-	var conf dojoConfig
+	var conf config.DojoConfig
+
+	// Setup ENV variables
+	viper.SetEnvPrefix("DD")
+	replace := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replace)
+	viper.AutomaticEnv()
+
+	// DEBUG
+	//os.Setenv("DD_DOJO_VER", "6.6.6")
 
 	err = viper.ReadInConfig()
 	if err != nil {
@@ -45,11 +53,23 @@ func main() {
 	if err != nil {
 		panic("TODO: Better error handling 02")
 	}
-	fmt.Printf("conf.install.ddDojoVer = %+v\n", conf.install.ddDojoVer)
-	fmt.Printf("conf = %+v\n", conf)
-	fmt.Printf("conf.install = %+v\n", conf.install)
+	fmt.Printf("conf.Install.Version = %+v\n\n", conf.Install.Version)
+	fmt.Printf("conf = %+v\n\n", conf)
+	fmt.Printf("conf.Install = %+v\n\n", conf.Install)
+	fmt.Printf("viper.Get(\"DojoVer\") = %+v\n", viper.Get("install.version"))
+	fmt.Printf("viper.AllKeys = %+v\n\n", viper.AllKeys())
+	fmt.Printf("viper.AllSettings = %+v\n\n", viper.AllSettings())
+	fmt.Printf("FROM VIPER viper.Get(\"install.db.engine\") = %+v\n\n", viper.Get("install.db.engine"))
+	fmt.Printf("FROM STRUCT config.Install.db.engine= %+v\n", conf.Install.DB.Engine)
 	// Stopped here
-	fmt.Println(testConfig())
+	fmt.Println(config.TestConfig())
+
+	// DEBUG - Testing writing config
+	err = viper.WriteConfigAs("runtime-install-config.yml")
+	if err != nil {
+		fmt.Printf("err from write was: %+v\n", err)
+		//panic("TODO: Better error handling 03")
+	}
 
 	// Start stub'ing out stuff
 	// Look at setup.bash's high-level workflow
